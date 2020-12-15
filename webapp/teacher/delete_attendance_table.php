863a57c043a0d103f465052307bd8609d259e5a3
thanhnguyen <ngtruongthanh98@gmail.com>
2020-12-15 20:54:01 +0700

    Crud attendance for teacher side

---
+++ webapp/teacher/delete_attendance_table.php
@@ -0,0 +1,14 @@
+<?php
+//including the database connection file
+include("../config.php");
+
+//getting id of the data from url
+$id_atd = $_GET['id_atd'];
+
+//deleting the row from table
+$result = mysqli_query($link, "DELETE FROM attendance_table WHERE id_atd=$id_atd");
+
+//redirecting to the display page (index.php in our case)
+header("Location:view_attendance.php");
+?>
+
